68995a76abd502590651d0263a46a7991bb8fbcf
Klajdi G <klajdi.gjonaj@gmail.com>
2020-05-05 13:35:40 -0400

    Changed to support new Markov implementation

--- midi_creator.py
+++ midi_creator.py
@@ -11,7 +11,7 @@ class Midi_Creator:
 		self.tempo = 140	# in BPM
 		self.volume = 100
 
-	def create_mid(self, note_stream, duration_stream, norm = True):
+	def create_mid(self, note_stream, duration_stream):
 		"""
 		Given a list of notes and durations, creates
 		and saves a mid file to self.mid with that
@@ -19,18 +19,17 @@ class Midi_Creator:
 		note_stream and duration_stream must be of the
 		same length.
 		"""
-		if norm:
-			n_constant = 12
-		else:
-			n_constant = 1
 		self.mid.addTempo(self.track, self.time, self.tempo)
 
 		for i, pitch in enumerate(note_stream):
 			duration = duration_stream[i]
-			# print("duration =", duration)
-			self.mid.addNote(self.track, self.channel, pitch,
-							self.time, duration/n_constant, self.volume)
-			self.time += duration/n_constant
+			if duration != 0:
+				self.mid.addNote(self.track, self.channel, pitch,
+								self.time, duration, self.volume)
+				self.time += duration
+			else:
+				self.time += 1
+		# self.mid.removeDuplicates()
 
 	def get_mid(self):
 		"""
@@ -54,4 +53,3 @@ if __name__ == '__main__':
 	creator = Midi_Creator()
 	creator.create_mid(note_stream, duration_stream)
 	creator.save('creator_test.mid')
-
